9b0b408e44dab416af54cd741c70f9f0b01866fc
Konnor <konnor.frik666@gmail.com>
2025-05-18 21:24:31 +0500

    model/User;db/postgres: add connection to postgres and rewrite model/user func for work with db

--- user_service/model/postgres/db.go
+++ user_service/model/postgres/db.go
@@ -1,6 +1,205 @@
 /*
-
+Connection manager
+Package for store database object as singletone
+Has auto reconnection, connection retries will be until connect is success
 */
 package postgres
 
+import (
+	// "log"
+	// "sync"
+	// "sync/atomic"
+	// "time"
+    "errors"
+    "fmt"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+    // DbDefaultSslmode = "disable"
+    // DbDefaultTimezone = "Asia/Yekaterinburg"
+
+    // reconnectDelay = time.Minute
+)
+
+// const (
+//     stateNotConnected int32 = iota
+//     stateConnected
+// )
+
+var (
+    // dbSingletone DB
+    // dbConfig DbConfig
+
+    // reconnectRequest chan struct{}
+    // stopReconnect chan struct{}
+
+    // state int32 
+
+    ErrInvalidConfig = errors.New("invalid database configuration")
+)
+
+// func init() {
+//     reconnectRequest = make(chan struct{}, 1)
+//     stopReconnect = make(chan struct{})
+// }
+
+// type DB struct {
+//     sync.Mutex
+//     DB *gorm.DB
+// }
+
+// SaveConfig - save configuration for connect to database.
+// It will be used by connection retrier
+// func SaveConfig(config DbConfig) error {
+//     if !config.isValid() {
+//         return ErrInvalidConfig
+//     }
+//
+//     dbConfig = config
+//     log.Printf("[PostgreSQL/SaveConfig]: config saved: %q\n", config.String())
+//     return nil
+// }
+
+// Connect - connect to db with saved config
+func Connect(config DbConfig) (*gorm.DB, error) {
+    // select {
+    // case reconnectRequest <- struct{}{}:
+    // default:
+    // }
+
+    if !config.isValid() {
+        return nil, ErrInvalidConfig
+    }
+
+    var db *gorm.DB
+    var err error
+    connectionInfo := config.String()
+    db, err = gorm.Open(postgres.Open(connectionInfo), &gorm.Config{})
+
+    if err != nil {
+        return nil, err
+    }
+
+    return db, nil
+}
+
+// func Get() *DB {
+//     dbSingletone.Lock()
+//     defer dbSingletone.Unlock()
+//
+//     if dbSingletone.DB == nil {
+//         if atomic.CompareAndSwapInt32(&state, stateNotConnected, stateConnected) {
+//             log.Printf("[PostgreSQL/Get]: DB is nil, switch state and run retrier\n")
+//             go connectionRetrier(reconnectRequest, stopReconnect)
+//
+//         } else {
+//             log.Printf("[PostgreSQL/Get]: DB is nil, send reconnect request\n")
+//             select {
+//             case reconnectRequest <- struct{}{}:
+//             default:
+//             }
+//         }
+//     }
+//
+//     return &dbSingletone
+// }
+
+// func CloseConnection() {
+//     select {
+//     case stopReconnect <- struct{}{}:
+//     default:
+//     }
+//
+//     dbSingletone.Lock()
+//     defer dbSingletone.Unlock()
+//
+//     if dbSingletone.DB != nil {
+//         if atomic.CompareAndSwapInt32(&state, stateConnected, stateNotConnected) {
+//             sql, err := dbSingletone.DB.DB()
+//
+//             if err == nil {
+//                 sql.Close()
+//                 dbSingletone.DB = nil
+//                 log.Printf("[PostgreSQL/CloseConnection]: Close DB connection\n")
+//
+//             } else {
+//                 log.Printf("[PostgreSQL/CloseConnection]: Error on get DB: %q\n", err)
+//             }
+//         }
+//     }
+// }
+
+// func connectionRetrier(reconnect, shutdown <-chan struct{}) {
+//     ticker := time.NewTicker(reconnectDelay)
+//     var err error
+//
+//     for {
+//         select {
+//         case <-ticker.C:
+//             dbSingletone.Lock()
+//
+//             if atomic.LoadInt32(&state) == stateConnected && dbSingletone.DB == nil {
+//                 log.Printf("[PostgreSQL/retrier]: DB is not connected. Try to connect\n")
+//                 dbSingletone.DB, err = gorm.Open(postgres.Open(dbConfig.String()), &gorm.Config{})
+//
+//                 if err != nil {
+//                     log.Printf("[PostgreSQL/retrier]: Erron on reconnect: %q\n", err)
+//                 }
+//             }
+//
+//             dbSingletone.Unlock()
+//
+//         case <-reconnect:
+//             dbSingletone.Lock()
+//             log.Printf("[PostgreSQL/retrier]: Got reconnect request\n")
+//             dbSingletone.DB, err = gorm.Open(postgres.Open(dbConfig.String()), &gorm.Config{})
+//
+//             if err != nil {
+//                 log.Printf("[PostgreSQL/retrier]: Erron on reconnect: %q\n", err)
+//             }
+//
+//             dbSingletone.Unlock()
+//
+//         // kill goroutine
+//         case <-shutdown:
+//             log.Printf("[PostgreSQL/retrier]: shutdown\n")
+//             return
+//         }
+//     }
+// }
+
+type DbConfig struct {
+    Host string
+    User string 
+    Password string 
+    DbName string 
+    Port string
+    SSLMode bool
+    TimeZone string
+}
+
+func (dc DbConfig) String() string {
+    sslMode := "disable"
+
+    if dc.SSLMode {
+        sslMode = "enable"
+    }
+
+    return fmt.Sprintf(
+        "host=%s database=%s user=%s password=%s port=%s sslmode=%s TimeZone=%s",
+        dc.Host,
+        dc.DbName,
+        dc.User,
+        dc.Password,
+        dc.Port,
+        sslMode,
+        dc.TimeZone,
+    )
+}
+
+func (dc DbConfig) isValid() bool {
+    return dc.Host != "" && dc.User != "" && dc.Password != "" && dc.Port != "" && dc.TimeZone != "" 
+}
--- user_service/model/user.go
+++ user_service/model/user.go
@@ -1,48 +1,124 @@
 package model
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"os"
 
-var (
-    users = map[string]*User{}
-    userToken = map[string]string{} // login to token
+	db "gophermart/model/postgres"
+
+	"gorm.io/gorm"
 )
 
 var (
-    ErrUserAlreadyExist = errors.New("user already exist")
-    ErrUserDoesNotExist = errors.New("user does not exist")
+	config = db.DbConfig{
+		Host:     os.Getenv("DB_HOST"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		DbName:   os.Getenv("DB_DB"),
+		Port:     os.Getenv("DB_PORT"),
+		TimeZone: "Asia/Yekaterinburg",
+	}
+
+	dbObj *gorm.DB
+)
+
+var (
+	ErrUserAlreadyExist = errors.New("user already exist")
+	ErrUserDoesNotExist = errors.New("user does not exist")
+    ErrDataBaseNotConnected = errors.New("database is not connected")
 )
 
 type User struct {
-    Login string
-    Password string
-    Token string
+	Login    string `gorm:"unique"`
+	Password string
+	Token    string
+}
+
+func init() {
+	connectToPostgres()
+}
+
+// connect to postgres and make automigrate
+func connectToPostgres() {
+	var err error
+	dbObj, err = db.Connect(config)
+
+	if err != nil {
+		if errors.Is(err, db.ErrInvalidConfig) {
+			log.Printf("[model:User]: Config is invalid: %q\n", config)
+
+		} else {
+			// need to run reconnector
+		}
+		return
+	}
+
+	err = dbObj.AutoMigrate(User{})
+
+	if err != nil {
+		log.Printf("[model:User]: Error on migrate: %q\n", err)
+		return
+	}
+
+	log.Printf("[model:User]: Database connected\n")
 }
 
 func NewUser(login, passw, tok string) (*User, error) {
-    if _, exist := users[login]; exist {
-        return nil, ErrUserAlreadyExist
+    if dbObj == nil {
+        connectToPostgres()
+        return nil, ErrDataBaseNotConnected
     }
 
-    user := &User{
+    var user User = User{
         Login: login,
         Password: passw,
         Token: tok,
     }
+    tx := dbObj.Model(User{}).FirstOrCreate(&user, user)
 
-    users[login] = user
-    return user, nil
+    if tx.Error != nil {
+        log.Printf("[model:User/NewUser]: Error on FirstOrCreate: %q\n", tx.Error)
+        return nil, tx.Error
+    }
+
+    return &user, nil
 }
 
 func UserByLogin(login string) (*User, error) {
-    user, ok := users[login]
-
-    if !ok {
-        return nil, ErrUserDoesNotExist
+    if dbObj == nil {
+        connectToPostgres()
+        return nil, ErrDataBaseNotConnected
     }
 
-    return user, nil
+    var user User
+    tx := dbObj.Model(User{}).First(&user, "login = ?", login)
+
+    if tx.Error != nil {
+        log.Printf("[model:User/UserByLogin]: Error: %q\n", tx.Error)
+
+        if tx.Error == gorm.ErrRecordNotFound {
+            return nil, ErrUserDoesNotExist
+        }
+
+        return nil, tx.Error
+    }
+
+    return &user, nil
 }
 
-func DeleteUser(login string) {
-    delete(users, login)
+func DeleteUser(user *User) error {
+    if dbObj == nil {
+        connectToPostgres()
+        return ErrDataBaseNotConnected
+    }
+
+    tx := dbObj.Model(User{}).Delete(user, "login = ?", user.Login)
+
+    if tx.Error != nil {
+        log.Printf("[model:User/DeleteUser]: Error on delete %q\n", tx.Error)
+        return tx.Error
+    }
+
+    return nil
 }
