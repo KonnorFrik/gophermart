ce7f7fc4edbcd20a527b30de190af0d567013d1e
Konnor <konnor.frik666@gmail.com>
2025-05-21 01:31:22 +0500

    api/test: add new func for run chain

--- api_test/api_test.go
+++ api_test/api_test.go
@@ -1,6 +1,7 @@
 package apitest_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
@@ -19,35 +20,29 @@ var (
     address = fmt.Sprintf("%s://%s:%d", proto, ip, port)
 )
 
-func TestHello(t *testing.T) {
-    response, err := client.R().Get(address + "/hello")
+type chainFunc func(*testing.T, string) string
 
-    if err != nil {
-        t.Fatalf("Got = %v\n", err)
+// Run testUserRegister for get token
+// pass token to all given funcs and overwrite it with return value (may be same)
+// at end call testUserDelete
+func runChain(t *testing.T, chain ...chainFunc) {
+    token := testUserRegister(t)
+
+    for _, f := range chain {
+        token = f(t, token)
     }
 
-    var statusCode int = response.StatusCode()
-    var wantStatus int = 200
-
-    if statusCode != wantStatus {
-        t.Fatalf("Got = %d, Want = %d\n", statusCode, wantStatus)
-    }
-
-    var body string = string(response.Body())
-    var wantBody string = `{"cookie":"NotSet","status":"OK"}`
-
-    if body != wantBody {
-        t.Fatalf("Got = %q, Want = %q\n", body, wantBody)
-    }
+    testUserDelete(t, token)
 }
 
-func TestCRUD(t *testing.T) {
-    cooks := testUserRegister(t)
-    cooks = testUserLogin(t, cooks)
-    testUserDelete(t, cooks)
+func TestUserCRUD(t *testing.T) {
+    token := testUserRegister(t)
+    token = testUserLogin(t, token)
+    testUserDelete(t, token)
 }
 
-func testUserRegister(t *testing.T) []*http.Cookie {
+// return a token string after registration
+func testUserRegister(t *testing.T) string {
     req := client.R()
     req.SetBody(map[string]string{
         "login": "user",
@@ -66,16 +61,34 @@ func testUserRegister(t *testing.T) []*http.Cookie {
         t.Errorf("Got = %d, Want = %d\n", statusCode, wantStatus)
     }
 
-    return response.Cookies()
+    body := response.Body()
+
+    if len(body) == 0 {
+        t.Errorf("Got no body = len=%d\n", len(body))
+    }
+
+    var result = map[string]string{}
+
+    if err := json.Unmarshal(body, &result); err != nil {
+        t.Errorf("Can't Unmarshal = %q - %q\n", string(body), err)
+    }
+
+    token, ok := result["token"]
+
+    if !ok {
+        t.Errorf("Wrong body struct %v\n", result)
+    }
+
+    return token
 }
 
-func testUserLogin(t *testing.T, cookies []*http.Cookie) []*http.Cookie {
+func testUserLogin(t *testing.T, token string) string {
     req := client.R().SetBody(
         map[string]string{
             "login": "user",
             "password": "user",
         },
-    ).SetCookies(cookies)
+    ).SetHeader("Authorization", token)
 
     response, err := req.Post(address + "/api/user/login")
 
@@ -90,18 +103,36 @@ func testUserLogin(t *testing.T, cookies []*http.Cookie) []*http.Cookie {
         t.Errorf("Got = %d, Want = %d\n", statusCode, wantStatus)
     }
 
-    return response.Cookies()
+    body := response.Body()
+
+    if len(body) == 0 {
+        t.Errorf("Got no body = len=%d\n", len(body))
+    }
+
+    var result = map[string]string{}
+
+    if err := json.Unmarshal(body, &result); err != nil {
+        t.Errorf("Can't Unmarshal = %q - %q\n", string(body), err)
+    }
+
+    token, ok := result["token"]
+
+    if !ok {
+        t.Errorf("Wrong body struct %v\n", result)
+    }
+
+    return token
 }
 
-func testUserDelete(t *testing.T, cookies []*http.Cookie) {
+func testUserDelete(t *testing.T, token string) {
     req := client.R().SetBody(
         map[string]string{
             "login": "user",
             "password": "user",
         },
-    ).SetCookies(cookies)
+    ).SetHeader("Authorization", token)
 
-    response, err := req.Post(address + "/api/user/delete")
+    response, err := req.Delete(address + "/api/user/delete")
 
     if err != nil {
         t.Errorf("Got = %v\n", err)
@@ -113,4 +144,31 @@ func testUserDelete(t *testing.T, cookies []*http.Cookie) {
     if statusCode != wantStatus {
         t.Errorf("Got = %d, Want = %d\n", statusCode, wantStatus)
     }
+}
+
+func TestOrderCRUD(t *testing.T) {
+    runChain(
+        t,
+        testUserLogin,
+        testCreateOrder,
+    )
+}
+
+func testCreateOrder(t *testing.T, token string) string {
+    req := client.R().SetHeader("Authorization", token)
+    req = req.SetBody("1234")
+    response, err := req.Post(address + "/api/user/orders")
+
+    if err != nil {
+        t.Errorf("Got = %v\n", err)
+    }
+
+    var statusCode int = response.StatusCode()
+    var wantStatus int = http.StatusAccepted
+
+    if statusCode != wantStatus {
+        t.Errorf("Got = %d, Want = %d\n", statusCode, wantStatus)
+    }
+
+    return token
 }
