f01c83acbb276124a1fe74bc7dee1d6decb58686
Konnor <konnor.frik666@gmail.com>
2025-05-21 01:35:12 +0500

    api: new endpoints registered

--- user_service/main.go
+++ user_service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
     "gophermart/api"
+    "gophermart/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,10 +14,14 @@ func main() {
     apiGroup := router.Group("/api")
     userGroup := apiGroup.Group("/user")
 
-    router.GET("/hello", api.Hello)
-    userGroup.POST("/register", api.Register)
-    userGroup.POST("/login", api.Login)
-    userGroup.POST("/delete", api.Delete)
+    userGroup.POST("/register", api.UserRegister)
+    userGroup.POST("/login", api.UserLogin)
+
+    UserAuthGroup := apiGroup.Group("/user")
+    UserAuthGroup.Use(middleware.JWTAuthenticate)
+    UserAuthGroup.DELETE("/delete", api.UserDelete)
+    UserAuthGroup.POST("/orders", api.NewOrder)
+    UserAuthGroup.GET("/orders", api.AllOrders)
 
     log.Println("[SERVER]: listen at:", ":8080")
     router.Run() 
