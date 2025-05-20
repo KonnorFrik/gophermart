19685dd08ae4ea93566418444ea8a2883e2fb7e1
Konnor <konnor.frik666@gmail.com>
2025-05-21 01:32:55 +0500

    middleware: move middleware into different file

---
+++ user_service/middleware/jwt_verify.go
@@ -0,0 +1,92 @@
+package middleware
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+
+var (
+    jwtSecretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+    ErrInvalidToken = errors.New("invalid token")
+)
+
+func init() {
+    if len(jwtSecretKey) == 0 {
+        panic("jwt secret key is missing")
+    }
+}
+
+func JWTAuthenticate(c *gin.Context) {
+    authToken := c.GetHeader("Authorization")
+
+    if authToken == "" {
+        log.Printf("[jwtAuthMDLWR]: token is missed\n")
+        c.AbortWithStatus(http.StatusUnauthorized)
+        return
+    }
+
+    var err error
+    tok, err := verifyJWT(authToken)
+
+    if err != nil {
+        log.Printf("[jwtAuthMDLWR]: failed token verification\n")
+        c.AbortWithStatus(http.StatusUnauthorized)
+        return
+    }
+
+    mapClaims, ok := tok.Claims.(jwt.MapClaims)
+
+    if !ok {
+        log.Printf("[jwtAuthMDLWR]: Can't convert claims\n")
+        c.AbortWithStatus(http.StatusUnauthorized)
+        return
+    }
+    
+    userIDstr, err := mapClaims.GetSubject()
+    // {
+    //     uidStr, ok := mapClaims["sub"]
+    //     log.Printf("[jwtAuthMDLWR]: RAW ACCESS 'sub': %q, %t\n", uidStr, ok)
+    // }
+
+    if err != nil {
+        log.Printf("[jwtAuthMDLWR]: Can't get subject claim: %q\n", err)
+        c.AbortWithStatus(http.StatusUnauthorized)
+        return
+    }
+
+    userID, err := strconv.ParseUint(userIDstr, 10, strconv.IntSize)
+
+    if err != nil {
+        log.Printf("[jwtAuthMDLWR]: Can't convert id to int: %q\n", err)
+        c.AbortWithStatus(http.StatusUnauthorized)
+        return
+    }
+
+    log.Printf("[jwtAuthMDLWR]: Set user id into context: %d\n", userID)
+    c.Set("userID", uint(userID))
+    c.Next()
+}
+
+func verifyJWT(tokStr string) (*jwt.Token, error) {
+    token, err := jwt.Parse(tokStr, func(t *jwt.Token) (interface{}, error) {
+        return jwtSecretKey, nil
+    })
+
+    if err != nil {
+        return nil, err 
+    }
+
+    if !token.Valid {
+        return nil, ErrInvalidToken
+    }
+
+    return token, nil
+}
+
