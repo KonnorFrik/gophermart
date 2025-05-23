ee63fe4d055f675e241eeadd28bbcdd22a822f0d
Konnor <konnor.frik666@gmail.com>
2025-05-23 15:50:33 +0500

    db/migrations: add more constraints

--- migrations/20250522155833_create_order_table.up.sql
+++ migrations/20250522155833_create_order_table.up.sql
@@ -1,11 +1,11 @@
 create table if not exists orders (
     id BIGSERIAL,
-    accrual BIGINT CHECK ( accrual >= 0 ),
+    accrual BIGINT NOT NULL CHECK ( accrual >= 0 ),
     number TEXT UNIQUE NOT NULL,
     -- status can be a enum
-    status SMALLINT,
-    uploaded_at TIMESTAMP,
-    user_id BIGINT,
+    status SMALLINT NOT NULL,
+    uploaded_at TIMESTAMP NOT NULL,
+    user_id BIGINT NOT NULL,
 
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
